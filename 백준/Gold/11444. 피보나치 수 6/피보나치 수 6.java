7037b96e1498b27b7d54e02667feace7a7907edb
Moonki Kim <84397561+Kimmoongi0320@users.noreply.github.com>
2026-01-29 12:45:18 +0900

    [Gold II] Title: 피보나치 수 6, Time: 108 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import  java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        System.out.print(divide(n)[0][1]);
+
+    }
+
+    static long[][] multiplyMatrix(long[][] mat, long[][] mat2){
+        long[][] result = new long[2][2];
+
+        result[0][0] = (mat[0][0]*mat2[0][0] + mat[0][1]*mat2[1][0])%1000000007;
+        result[0][1] = (mat[0][0]*mat2[0][1] + mat[0][1]*mat2[1][1])%1000000007;
+        result[1][0] = (mat[1][0]*mat2[0][0] + mat[1][1]*mat2[1][0])%1000000007;
+        result[1][1] = (mat[1][0]*mat2[0][1] + mat[1][1]*mat2[1][1])%1000000007;
+
+        return result;
+    }
+
+    static long[][] divide(long times){
+        if(times == 1){
+            return new long[][] {{1,1},{1,0}};
+        }
+        long[][] mat = divide(times / 2);
+
+        if((times % 2) == 1){
+            long[][] answer = multiplyMatrix(mat,mat);
+            return multiplyMatrix(answer, new long[][] {{1,1},{1,0}});
+        }else{
+            return multiplyMatrix(mat,mat);
+        }
+    }
+}
+
+
