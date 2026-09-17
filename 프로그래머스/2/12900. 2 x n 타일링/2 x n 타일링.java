041d4b31531c8394f98d6329c1ee44c3dcc81f64
Moonki Kim <84397561+Kimmoongi0320@users.noreply.github.com>
2026-09-17 23:55:04 +0900

    [level 2] Title: 2 x n 타일링, Time: 1.24 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        if(n < 3) return dp[n];
+        
+        for(int i = 3; i < n+1; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1000000007;
+        }
+        return dp[n];
+    }
+}
