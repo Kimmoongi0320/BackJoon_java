6bf0403e55c7accf3d99bc564b523cf842c2242d
Moonki Kim <84397561+Kimmoongi0320@users.noreply.github.com>
2026-09-19 22:07:14 +0900

    [level 2] Title: 가장 큰 수, Time: 107.64 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] arr = new String[numbers.length];
+        
+        for(int i = 0; i < numbers.length; i++){
+            arr[i] = numbers[i] + "";
+        }
+        Arrays.sort(arr,(a,b)-> (b+a).compareTo(a+b));
+        if(arr[0].equals("0")) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(String num: arr){
+            sb.append(num);
+        }
+        return sb.toString();
+    }
+}
