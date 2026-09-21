97823d9ebece086cea8dae9be57367ae5c5841f3
Moonki Kim <84397561+Kimmoongi0320@users.noreply.github.com>
2026-09-21 11:23:36 +0900

    [level 2] Title: 시소 짝꿍, Time: 39.81 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        long answer = 0;
+
+        Arrays.sort(weights);
+
+        Map<Integer, Long> map = new HashMap<>();
+
+        for (int w : weights) {
+
+            // 1 : 1
+            answer += map.getOrDefault(w, 0L);
+
+            // 2 : 3
+            if (w % 3 == 0) {
+                answer += map.getOrDefault(w * 2 / 3, 0L);
+            }
+
+            // 1 : 2
+            if (w % 2 == 0) {
+                answer += map.getOrDefault(w / 2, 0L);
+            }
+
+            // 3 : 4
+            if (w % 4 == 0) {
+                answer += map.getOrDefault(w * 3 / 4, 0L);
+            }
+
+            map.put(w, map.getOrDefault(w, 0L) + 1);
+        }
+
+        return answer;
+    }
+}
